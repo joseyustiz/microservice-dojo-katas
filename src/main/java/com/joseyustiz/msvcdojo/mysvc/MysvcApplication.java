30d99a166deff15235b10beffa2a32dcb0d9bddc
Jose Angel Yustiz Jimenez <jose.yustiz@gmail.com>
2018-10-29 10:40:32 -0300

    Initial project

---
+++ src/main/java/com/joseyustiz/msvcdojo/mysvc/MysvcApplication.java
@@ -0,0 +1,12 @@
+package com.joseyustiz.msvcdojo.mysvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysvcApplication.class, args);
+    }
+}
