d56b095ace2fdeb0c8157676959a67fc8171e14a
Jose Angel Yustiz Jimenez <jose.yustiz@gmail.com>
2018-10-31 12:47:32 -0300

    Moved back to JUnit4

--- kata-3/src/test/java/mysvc/model/AccountTest.java
+++ kata-3/src/test/java/mysvc/model/AccountTest.java
@@ -1,18 +1,16 @@
 package mysvc.model;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created by jyustiz on 10/31/18 for project mysvc.
  */
-@DisplayName("An account")
 public class AccountTest {
 
     @Test
-    @DisplayName("converted to String")
-    void toStringReturnProperJsonFormat(){
+    public void toStringReturnProperJsonFormat(){
         Account account = new Account("Jose");
         String joseToString="User{id:null, username:\'Jose\'}";
         assertThat(account.toString()).isEqualTo(joseToString);
