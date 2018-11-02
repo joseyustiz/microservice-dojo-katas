39728a0ccb21ee93199bf4eda8428d873a0895e9
Jose Angel Yustiz Jimenez <jose.yustiz@gmail.com>
2018-11-02 15:31:57 -0300

    Initial configuration server module

---
+++ kata-4/config-server/src/main/java/com/joseyustiz/msvcdojo/configserver/ConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.joseyustiz.msvcdojo.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
