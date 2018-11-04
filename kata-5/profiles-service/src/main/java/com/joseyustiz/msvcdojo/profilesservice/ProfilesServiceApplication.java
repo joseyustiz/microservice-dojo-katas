909365bd15b14ac8cea6c3fc0af2fde9fc776ea6
Jose Angel Yustiz Jimenez <jose.yustiz@gmail.com>
2018-11-04 15:55:42 -0300

    Initial kata-5/profiles-service project

---
+++ kata-5/profiles-service/src/main/java/com/joseyustiz/msvcdojo/profilesservice/ProfilesServiceApplication.java
@@ -0,0 +1,12 @@
+package com.joseyustiz.msvcdojo.profilesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfilesServiceApplication.class, args);
+	}
+}
