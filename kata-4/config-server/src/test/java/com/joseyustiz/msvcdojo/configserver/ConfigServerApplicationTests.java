6f9be047d05bfa0d1a3c4dca7bb49b103570dcf9
Jose Angel Yustiz Jimenez <jose.yustiz@gmail.com>
2018-11-04 01:36:20 -0300

    Created a basic unit test to verigy that the server loaded

--- kata-4/config-server/src/test/java/com/joseyustiz/msvcdojo/configserver/ConfigServerApplicationTests.java
+++ kata-4/config-server/src/test/java/com/joseyustiz/msvcdojo/configserver/ConfigServerApplicationTests.java
@@ -2,7 +2,9 @@ package com.joseyustiz.msvcdojo.configserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -11,6 +13,10 @@ public class ConfigServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
-	}
+        try (ConfigurableApplicationContext context = new SpringApplicationBuilder(ConfigServerApplication.class)
+                .properties("spring.config.name=configserver").run("--server.port=0")) {
+        }
+
+    }
 
 }
