afb6feff79dba91cfbef664cb3bd1d5175b0b29b
Jose Angel Yustiz Jimenez <jose.yustiz@gmail.com>
2018-11-18 21:38:56 -0300

    Profile unit test

--- kata-5/profiles-service/src/test/java/com/joseyustiz/msvcdojo/profilesservice/model/ProfileTest.java
+++ kata-5/profiles-service/src/test/java/com/joseyustiz/msvcdojo/profilesservice/model/ProfileTest.java
@@ -1,7 +1,66 @@
 package com.joseyustiz.msvcdojo.profilesservice.model;
 
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * Created by jyustiz on 11/4/18 for project microservice-dojo-katas.
  */
 public class ProfileTest {
+    @Test
+    public void getCorrectToStringOfProfilesWithNullId(){
+        Profile profile = new Profile("Jose Yustiz");
+        String profileToString="Customer{id:null, fullName:\'Jose Yustiz\'}";
+        assertThat(profile.toString()).isEqualTo(profileToString);
+    }
+
+    @Test
+    public void getCorrectToStringOfProfilesWithNotNullId(){
+        Profile profile = new Profile("Jose Yustiz");
+        profile.setId("1");
+        String profileToString="Customer{id:'1', fullName:\'Jose Yustiz\'}";
+        assertThat(profile.toString()).isEqualTo(profileToString);
+    }
+
+    @Test
+    public void getAmountPhotoOfAProfile(){
+        Profile profile = new Profile("Jose Yustiz");
+        profile.setId("1");
+
+        profile.addPhotoReference("photo.jpg");
+        assertThat(profile.getPhotoCount()).isEqualTo(1);
+    }
+
+    @Test
+    public void setKeyUpdateTheIdOfTheProfile(){
+        Profile profile = new Profile("Jose Yustiz");
+        profile.setKey("a");
+
+        assertThat(profile.getKey()).isEqualTo("a");
+    }
+
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void fullNameCannotBeNull(){
+        Profile profile = new Profile(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void fullNameCannotBeBlank(){
+        Profile profile = new Profile("");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void keyCannotBeNull(){
+        Profile profile = new Profile("Jose Yustiz");
+        profile.setKey(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void keyCannotBeBlank(){
+        Profile profile = new Profile("Jose Yustiz");
+        profile.setKey("");
+    }
 }
